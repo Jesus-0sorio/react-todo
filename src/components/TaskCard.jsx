c18088871fc45b8578e3a31557c89d15fe7c1aa9
Jesus-0sorio <jaoz10.16@gmail.com>
2023-01-29 16:14:50 -0500

    add styles

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,19 +1,20 @@
-import React from 'react';
+import React from "react";
 
 export function TaskCard({ task, deleteTask }) {
   return (
-    <div className="" >
-      <h5>{task.title}</h5>
+    <div className=" shadow p-6 border border-gray-200 rounded-lg">
+      <h5 className="text-2xl lg:text-3xl p-1">{task.title}</h5>
       <textarea
         name="Contenido"
         id="Content"
-        rows="12"
+        rows="7"
         defaultValue={task.content}
+        className="resize-none w-full p-1 text-lg lg:text-xl"
         readOnly
       />
-      <div>
-        <button>Completar</button>
-        <button onClick={() => deleteTask(task.id)}>Eliminar</button>
+      <div className="grid grid-cols-2 gap-4">
+        <button className="text-center bg-green-500 rounded-lg text-md md:text-md text-white">Completar</button>
+        <button className="text-center rounded-lg px-3 py-1 bg-red-600 text-white text-md md:text-md" onClick={() => deleteTask(task.id)}>Eliminar</button>
       </div>
     </div>
   );
